dde028de5eadc91d8689656179d541af279b4848
RobotReece_999 <Reece7jr@outlook.com>
2025-11-25 21:00:49 -0600

    Changes from years ago.  I dont know what they are

---
+++ C++_Class/Exam_2/quiz2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string fred = "Cat";
+fred = fred.at(1) ="ab";
+int x;
+int y = 0;
+cin >> x;
+if (x = 20)
+{
+    y=3;
+    cout << y ;
+}
+
+
+}
